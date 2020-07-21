521482fbb24b3bd87f573a00598a0e54f3f3352b
unknown <tommyconner96@gmail.com>
2020-07-21 18:44:30 -0400

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,32 @@
+
 module.exports = {
-  client: "sqlite3",
-  useNullAsDefault: true,
-  connection: {
-    filename: "./database/auth.db3",
-  },
-  migrations: {
-    directory: "./database/migrations",
-  },
-  seeds: {
-    directory: "./database/seeds",
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run("PRAGMA foreign_keys = ON", done)
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/data.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
     },
   },
   production: {
     client: "pg",
     // useNullAsDefault: true,
     connection: process.env.DATABASE_URL,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
     migrations: {
       directory: "./database/migrations",
     },
@@ -31,4 +39,5 @@ module.exports = {
     //   },
     // },
   }
-}
+
+};
