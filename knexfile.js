20b9b681a13b6f81cc89df65116abb16c018feb6
unknown <tommyconner96@gmail.com>
2020-07-21 18:15:27 -0400

    added postgres

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,34 @@
 module.exports = {
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./database/auth.db3",
-	},
-	migrations: {
-		directory: "./database/migrations",
-	},
-	seeds: {
-		directory: "./database/seeds",
-	},
-	pool: {
-		afterCreate: (conn, done) => {
-			conn.run("PRAGMA foreign_keys = ON", done)
-		},
-	},
+  client: "sqlite3",
+  useNullAsDefault: true,
+  connection: {
+    filename: "./database/auth.db3",
+  },
+  migrations: {
+    directory: "./database/migrations",
+  },
+  seeds: {
+    directory: "./database/seeds",
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run("PRAGMA foreign_keys = ON", done)
+    },
+  },
+  production: {
+    client: "pg",
+    // useNullAsDefault: true,
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run("PRAGMA foreign_keys = ON", done)
+    //   },
+    // },
+  }
 }
