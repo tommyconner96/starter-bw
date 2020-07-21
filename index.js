ea20d6a616a01172250f321c1bfc250f05c425ce
unknown <tommyconner96@gmail.com>
2020-07-21 19:56:09 -0400

    access control allow origin

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ server.use(helmet())
 server.use(cookieParser())
 server.use(express.json())
 server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Origin", "https://starter-bw.herokuapp.com/" );
+    res.header("Access-Control-Allow-Origin", "https://starter-bw.herokuapp.com/auth/login" )
+    res.header("Access-Control-Allow-Origin", "https://starter-bw.herokuapp.com/auth/register" );;
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
-    res.header("Access-Control-Allow-Credentials", true);
+
     next();
   });
 server.use(cors())
