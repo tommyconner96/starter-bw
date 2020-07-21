efe96e42a7f18f4d0ad18d2f081ef6fcc4ac9b46
unknown <tommyconner96@gmail.com>
2020-07-21 19:50:06 -0400

    cors

--- index.js
+++ index.js
@@ -13,16 +13,13 @@ server.use(helmet())
 server.use(cookieParser())
 server.use(express.json())
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", req.headers.origin );
+    res.header("Access-Control-Allow-Origin", "https://starter-bw.herokuapp.com/" );
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
     res.header("Access-Control-Allow-Credentials", true);
     next();
   });
-server.use(cors({
-	credentials: true,
-	origin: 'https://elastic-jackson-7f8963.netlify.app/',
-}))
+server.use(cors())
 
 server.use("/auth", authRouter)
 server.use("/coffee", restrict(), coffeeRouter)
