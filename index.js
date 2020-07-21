759845b6a411f486eec39771512ca58567fbccbf
unknown <tommyconner96@gmail.com>
2020-07-21 19:33:07 -0400

    asdasda

--- index.js
+++ index.js
@@ -6,40 +6,34 @@ const authRouter = require("./auth/auth-router")
 const coffeeRouter = require("./coffee/coffee-router")
 const restrict = require("./middleware/restrict")
 
-// require('dotenv').config(); // add this line as the first thing to run1
 const server = express()
 const port = process.env.PORT || 5000
-
-
 
 server.use(helmet())
 server.use(cookieParser())
 server.use(express.json())
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'https://elastic-jackson-7f8963.netlify.app')
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie")
-    res.header("Access-Control-Allow-Credentials", true)
-    next()
-})
-server.use(cors())
+
+server.use(cors({
+	credentials: true,
+	origin: 'https://elastic-jackson-7f8963.netlify.app/',
+}))
 
 server.use("/auth", authRouter)
 server.use("/coffee", restrict(), coffeeRouter)
 
 server.get("/", (req, res, next) => {
-    res.json({
-        message: "Welcome to our API",
-    })
+	res.json({
+		message: "Welcome to our API",
+	})
 })
 
 server.use((err, req, res, next) => {
-    console.log(err)
-    res.status(500).json({
-        message: "Something went wrong",
-    })
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
 })
 
 server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`)
+	console.log(`Running at http://localhost:${port}`)
 })
