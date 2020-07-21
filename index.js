00b2df405254ce9e08ed9a30ba90e1715ddb5c0e
unknown <tommyconner96@gmail.com>
2020-07-21 19:12:57 -0400

    fixed cors issue

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const port = process.env.PORT || 5000
 server.use(helmet())
 server.use(cookieParser())
 server.use(express.json())
-
-server.use(cors({
-	credentials: true
-}))
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+  });
+server.use(cors())
 
 server.use("/auth", authRouter)
 server.use("/coffee", restrict(), coffeeRouter)
