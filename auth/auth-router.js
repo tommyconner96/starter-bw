f23ee84c2c8d803f9625813db1b0fcb41479a011
unknown <tommyconner96@gmail.com>
2020-07-22 18:57:36 -0400

    WORKS LOCALLY

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,135 +24,30 @@ router.post("/register", async (req, res, next) => {
 	}
 })
 
-router.post("/login", async (req, res, next) => {
-	try {
-		const { username, password } = req.body
-		const user = await authModel.findByUsername(username).first()
-		const passwordValid = await bcrypt.compare(password, user.password)
-
-		if (!passwordValid) {
-			return res.status(401).json({
-				message: "Invalid Credentials",
-			})
-		}
-
-		if (user) {
-			req.session.user = user
-			console.log(req.session.user)
-			res.status(200).json({
-				message: `Welcome ${user.username}!`,
-			})
-		} else {
-			return res.status(401).json({
-				message: "Invalid Credentials",
-			})
-		}
-
-		// // hash the password again and see if it matches what we have in the database
-
-
-		// if (!passwordValid) {
-		// 	return res.status(401).json({
-		// 		message: "Invalid Credentials",
-		// 	})
-		// }
-
-		// // generate a new session for this user,
-		// // and sends back a session ID
-		// req.session.user = user
-		// console.log(res.session.user = user)
-
-		// res.json({
-		// 	message: `Welcome ${user.username}!`,
-		// })
-	} catch (err) {
-		next(err)
-	}
-})
-
-// router.post("/login", async (req, res, next) => {
-// 	const authError = {
-// 		message: "Invalid Credentials",
-// 	}
-
-// 	try {
-// 		const user = await authModel.findByUsername(req.body.username)
-// 		if (!user) {
-// 			return res.status(401).json(authError)
-// 		}
-
-// 		const passwordValid = await bcrypt.compare(req.body.password, user.password)
-// 		if (!passwordValid) {
-// 			return res.status(401).json(authError)
-// 		}
-
-// 		// create a new session in the database
-// 		const session = await authModel.add({
-// 			user_id: user.id,
-// 			// a SQLite trick to set a date in the future
-// 			expires: db.raw("DATETIME('now', 'localtime', '+1 hour')"),
-// 		})
-
-// 		// add the session details and other user details to the token payload
-// 		const token = jwt.sign({
-// 			sessionId: session.id,
-// 			username: user.username,
-// 		}, 'secret secret')
-
-// 		res.cookie("token", token)
-// 		req.session.user = user
-// 		res.json({
-// 			message: `Welcome ${user.username}!`, token
-// 		})
-// 	} catch(err) {
-// 		next(err)
-// 	}
-// })
-
-// router.post('/login', (req, res, next) => {
-// 	const { username, password } = req.body
-// 	authModel
-// 		.findByUsername(username)
-// 		.then(user => {
-// 			if (user && bcrypt.compareSync(password, user.password)) {
-// 				//adds jwt token
-// 				const token = createToken(user)
-// 				req.session.user = user
-// 				res.cookie("token", token)
-// 				//adds session 
-// 				res.status(200).json({ message: `Hi ${user.username}! have a token:`, token })
-// 			} else {
-// 				res.status(401).json({ message: "Invalid credentiels" })
-// 			}
-// 		})
-// 		.catch(err => next(err))
-// })
-
-// router.get("/logout", restrict, async (req, res, next) => {
-// 	try {
-// 		await authModel.deleteById(req.session.id)
-
-// 		res.cookie("token", "", { maxAge: 0 })
-// 		res.status(204).end()
-// 	} catch (err) {
-// 		next(err)
-// 	}
-// })
-
-router.get("/logout", async (req, res, next) => {
-	try {
-		req.session.destroy((err) => {
-			if (err) {
-				next(err)
+router.post('/login', (req, res, next) => {
+	const { username, password } = req.body
+	authModel
+		.findByUsername(username)
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				//adds jwt token
+				const token = createToken(user)
+				// req.session.user = user
+				res.cookie("token", token)
+				//adds session 
+				res.status(200).json({ message: `Hi ${user.username}! have a token:`, token })
 			} else {
-				res.status(204).end()
+				res.status(401).json({ message: "Invalid credentiels" })
 			}
 		})
-	} catch (err) {
-		next(err)
-	}
+		.catch(err => next(err))
 })
 
+
+router.get("/logout", async (req, res, next) => {
+    res.clearCookie("token");
+    res.json({message: "you have successfully logged out"})
+})
 
 function createToken(user) {
 	const payload = {
--- index.js
+++ index.js
@@ -1,61 +1,45 @@
 const express = require("express")
 const helmet = require("helmet")
 const cookieParser = require("cookie-parser")
-const session = require('express-session')
-const KnexSessionStore = require('connect-session-knex')(session)
 const cors = require("cors")
 const authRouter = require("./auth/auth-router")
 const coffeeRouter = require("./coffee/coffee-router")
 const restrict = require("./middleware/restrict")
-const bodyParser = require('body-parser')
+
 const server = express()
 const port = process.env.PORT || 5000
-const db = require('./database/data')
 
+server.use(helmet())
+server.use(cookieParser())
+server.use(express.json())
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'https://elastic-jackson-7f8963.netlify.app' );
+    res.header("Access-Control-Allow-Origin", req.headers.origin );
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
     res.header("Access-Control-Allow-Credentials", true);
     next();
   });
-server.use(helmet())
-server.use(cors())
-server.use(express.json())
-
-  //whitelist domain for cookies
-  
-  server.use(cors({
-    credentials: true,
-    origin: 'https://elastic-jackson-7f8963.netlify.app'
-  }));
-server.use(session({
-	resave: false, // avoids recreating sessions that have not changed
-	saveUninitialized: false, // comply with GDPR laws
-    secret: "keep it secret, keep it safe",
-    name: 'sessionCookie',
-    cookie: {
-      maxAge: 1000 * 60 * 60,
-    //   secure: true,
-      httpOnly: true
-    },
-	// store: new KnexSessionStore({
-	// 	knex: db, // configured instance of Knex, or the live database connection
-	// 	createtable: true, // if the session table does not exist, create it
-	// }),
+server.use(cors({
+	credentials: true,
+	origin: 'http://localhost:3000',
 }))
-
 
 server.use("/auth", authRouter)
 server.use("/coffee", restrict(), coffeeRouter)
 
 server.get("/", (req, res, next) => {
-    res.json({
-        message: "Welcome to our API",
-    })
+	res.json({
+		message: "Welcome to our API",
+	})
 })
 
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 server.listen(port, () => {
-    console.log(`Running at http://localhost:${port}`)
+	console.log(`Running at http://localhost:${port}`)
 })
