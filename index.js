a44f4fe7c6b00f2f53f1acd23910aaf0ea95d996
unknown <tommyconner96@gmail.com>
2020-08-01 17:41:42 -0400

    full CRUD done ready to deply

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ const restrict = require("./middleware/restrict")
 const server = express()
 const port = process.env.PORT || 5000
 
-require('dotenv').config()
+// require('dotenv').config()
 
 server.use(helmet())
 server.use(cookieParser())
 server.use(express.json())
-// server.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", 'http://localhost:3000' );
-//     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie");
-//     res.header("Access-Control-Allow-Credentials", true);
-//     next();
-//   });
-// server.use(cors({
-// 	credentials: true,
-// 	origin: 'http://localhost:3000',
-// }))
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", 'https://boring-visvesvaraya-e96371.netlify.app')
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Set-Cookie")
+	res.header("Access-Control-Allow-Credentials", true)
+	next()
+})
+server.use(cors({
+	credentials: true,
+	origin: 'https://boring-visvesvaraya-e96371.netlify.app',
+}))
 
 server.use("/auth", authRouter)
 server.use("/users/", restrict(), coffeeRouter)
